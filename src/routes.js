aa020e06dd660c6a9162cc316ff4f4cb4525e9fc
marlondck <15050029+marlondck@users.noreply.github.com>
2018-09-09 16:57:53 -0400

    page repository e icon logout

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import { metrics } from 'styles';
+import HeaderRight from 'components/HeaderRight';
 
+//pages
 import Welcome from 'pages/welcome';
 import Repositories from 'pages/repositories';
 import Organizations from 'pages/organizations';
@@ -15,6 +19,12 @@ const createNavigator = (isLogged = false) =>
     },
   }, {
     initialRouteName: isLogged ? 'User' : 'Welcome',
+    navigationOptions: {
+      headerStyle: {
+        paddingHorizontal: metrics.basePadding,
+      },
+      headerRight: <HeaderRight/>,
+    },
   });
 
 export default createNavigator;
