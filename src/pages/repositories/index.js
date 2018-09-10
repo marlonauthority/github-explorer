5176b3de812211b4a4de69000d11ab1913200de8
Marlon Authority <marlonauthority@Marlons-MacBook-Pro.local>
2018-09-10 17:16:49 -0400

    inicio da chamada api repositories

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
+import api from 'services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View } from 'react-native';
+import {
+    View,
+    Text,
+    AsyncStorage,
+    ActivityIndicator,
+    FlatList,
+} from 'react-native';
+import styles from './styles';
 
 export default class Repositories extends Component {
   static navigationOptions = {
     title: 'Repositórios',
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
-
   };
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data: response.data, loading: false })
+  }
+
+  renderListItem  = ({ item }) => (
+    <Text>
+      { item.full_name }
+    </Text>
+  )
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
+      </View>
+    )
   }
 }
