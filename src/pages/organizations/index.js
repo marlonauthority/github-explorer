0a4d9f6b1c4c4b6cc40fe480a76b66b601c50580
marlondck <15050029+marlondck@users.noreply.github.com>
2018-09-12 16:41:05 -0400

    organizations e imagem

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import api from 'services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+import OrganizationItem from './components/OrganizationItem';
+import styles from './styles';
+
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -9,7 +19,41 @@ export default class Organizations extends Component {
 
   };
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({ data: response.data, loading: false })
+  }
+
+  renderListItem  = ({ item }) => <OrganizationItem organization={item} />
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+      numColumns={2}
+      columnWrapperStyle={styles.columnContainer}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
+      </View>
+    )
   }
 }
