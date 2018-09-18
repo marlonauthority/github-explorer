996fcea80f1832ceb29ea123d44312458292c554
Marlon Authority <marlonauthority@Marlons-MacBook-Pro.local>
2018-09-18 15:31:54 -0400

    finish him

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -20,6 +20,7 @@ export default class Repositories extends Component {
   state = {
     data: [],
     loading: true,
+    refreshing: false,
   }
 
   componentDidMount() {
@@ -27,10 +28,16 @@ export default class Repositories extends Component {
   }
 
   loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
     const username = await AsyncStorage.getItem('@Githuber:username');
     const response = await api.get(`/users/${username}/repos`);
 
-    this.setState({ data: response.data, loading: false })
+    this.setState({
+      data: response.data,
+      loading: false,
+      refreshing: false,
+     })
   }
 
   renderListItem  = ({ item }) => <RepositoryItem repository={item} />
@@ -40,6 +47,8 @@ export default class Repositories extends Component {
       data={this.state.data}
       keyExtractor={item => String(item.id)}
       renderItem={this.renderListItem}
+      onRefresh={this.loadRepositories}
+      refreshing={this.state.refreshing}
     />
   )
 
